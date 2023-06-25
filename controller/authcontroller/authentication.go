3d0f824b3c7b38f92f9ceafb9f03110c1051763a
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-25 17:25:01 +0700

    Add Login and Register user

---
+++ controller/authcontroller/authentication.go
@@ -0,0 +1,58 @@
+package authcontroller
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sudrajad02/ecommerce/database"
+	"github.com/sudrajad02/ecommerce/models"
+)
+
+func Login(c *fiber.Ctx) error {
+	payload := struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}{}
+
+	if err := c.BodyParser(&payload); err != nil {
+		return err
+	}
+
+	if payload.Username == "" || payload.Password == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Please fill username and password",
+		})
+	}
+
+	var account models.Account
+
+	if err := database.DB.First(&account, "account_username = ? and account_password = ?", payload.Username, payload.Password).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	// claims := jwt.MapClaims{
+	// 	"name":  "John Doe",
+	// 	"admin": true,
+	// 	"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	// }
+
+	return c.JSON(account)
+}
+
+func Register(c *fiber.Ctx) error {
+	var account models.Account
+
+	if err := c.BodyParser(&account); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	if err := database.DB.Save(&account).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	return c.JSON(account)
+}
