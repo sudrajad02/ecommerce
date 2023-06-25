81a96b8161c3ddd7e929aaa032b9ffcbcb5b8472
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-25 17:25:50 +0700

    Add endpoint for login and register

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	accountcontroller "github.com/sudrajad02/ecommerce/controller/accountcontroller"
+	"github.com/sudrajad02/ecommerce/controller/authcontroller"
 	"github.com/sudrajad02/ecommerce/database"
 )
 
@@ -14,8 +15,11 @@ func main() {
 	app := fiber.New()
 
 	api := app.Group("/api")
+	auth := api.Group("/auth")
 	book := api.Group("/book")
 
+	auth.Post("/", authcontroller.Login)
+	auth.Post("/register/", authcontroller.Register)
 	book.Get("/", accountcontroller.Index)
 
 	app.Listen(":3000")
