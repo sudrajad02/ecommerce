a1e00080e857a67c5a35c4c848b47a2baa40ec75
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-25 18:58:37 +0700

    Add get list and detail product

---
+++ controller/productcontroller/productcontroller.go
@@ -0,0 +1,31 @@
+package productcontroller
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sudrajad02/ecommerce/database"
+	"github.com/sudrajad02/ecommerce/models"
+)
+
+func ListProduct(c *fiber.Ctx) error {
+	var product []models.Product
+
+	queryValue := c.Params("category_id")
+
+	if queryValue == "" {
+		database.DB.Find(&product)
+	} else {
+		database.DB.Where("category_id = ?", queryValue).Find(&product)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(product)
+}
+
+func DetailProduct(c *fiber.Ctx) error {
+	var product models.Product
+
+	queryValueProductId := c.Params("product_id")
+
+	database.DB.Where("product_id = ?", queryValueProductId).Find(&product)
+
+	return c.Status(fiber.StatusOK).JSON(product)
+}
