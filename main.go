8ce99067751ac7a8764826e7672668a87b62513c
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-25 18:59:48 +0700

    Add endpoint product list and detail product

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/sudrajad02/ecommerce/controller/authcontroller"
+	"github.com/sudrajad02/ecommerce/controller/productcontroller"
 	"github.com/sudrajad02/ecommerce/database"
 )
 
@@ -15,9 +16,15 @@ func main() {
 
 	api := app.Group("/api")
 	auth := api.Group("/auth")
+	product := api.Group("/product")
 
+	// auth
 	auth.Post("/", authcontroller.Login)
 	auth.Post("/register/", authcontroller.Register)
+
+	// product
+	product.Get("/:category_id?", productcontroller.ListProduct)
+	product.Get("/detail/:product_id", productcontroller.DetailProduct)
 
 	app.Listen(":3000")
 }
