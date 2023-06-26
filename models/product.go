99344a933335ca87042afa88c9a2ff8327853a75
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-26 23:13:55 +0700

    Add associations product to product category

--- models/product.go
+++ models/product.go
@@ -1,14 +1,15 @@
 package models
 
 type Product struct {
-	ProductId    int    `gorm:"primary_key;auto_increment" json:"id"`
-	ProductName  string `gorm:"type:varchar(64);null" json:"name"`
-	ProductColor string `gorm:"type:varchar(8);null" json:"color"`
-	ProductSize  string `gorm:"type:varchar(4);null" json:"size"`
-	ProductPrice string `gorm:"type:float;DEFAULT 0" json:"price"`
-	ProductStock string `gorm:"type:int(4);DEFAULT 0" json:"stock"`
-	CategoryId   string `gorm:"type:int;foreignKey:CategoryId" json:"category_id"`
-	CreatedAt    string `gorm:"type:timestamp;default:null" json:"created_at"`
-	UpdatedAt    string `gorm:"type:timestamp;default:null" json:"updated_at"`
-	DeletedAt    string `gorm:"type:timestamp;default:null" json:"deleted_at"`
+	ProductId         int             `gorm:"primary_key;auto_increment" json:"id"`
+	ProductName       string          `gorm:"type:varchar(64);null" json:"name"`
+	ProductColor      string          `gorm:"type:varchar(8);null" json:"color"`
+	ProductSize       string          `gorm:"type:varchar(4);null" json:"size"`
+	ProductPrice      string          `gorm:"type:float;DEFAULT 0" json:"price"`
+	ProductStock      string          `gorm:"type:int(4);DEFAULT 0" json:"stock"`
+	ProductCategoryId string          `gorm:"not null" json:"-"`
+	ProductCategory   ProductCategory `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:ProductCategoryId;references:CategoryId" json:"category"`
+	CreatedAt         string          `gorm:"type:timestamp;default:null" json:"created_at"`
+	UpdatedAt         string          `gorm:"type:timestamp;default:null" json:"updated_at"`
+	DeletedAt         string          `gorm:"type:timestamp;default:null" json:"deleted_at"`
 }
