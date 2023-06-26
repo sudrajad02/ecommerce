470ed57df362a10ef7f539f14a7f2657ebe825e7
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-27 00:37:01 +0700

    Add routes cart

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/sudrajad02/ecommerce/controller/authcontroller"
+	"github.com/sudrajad02/ecommerce/controller/cartcontroller"
 	"github.com/sudrajad02/ecommerce/controller/productcontroller"
 	"github.com/sudrajad02/ecommerce/database"
 )
@@ -17,6 +18,7 @@ func main() {
 	api := app.Group("/api")
 	auth := api.Group("/auth")
 	product := api.Group("/product")
+	cart := api.Group("/cart")
 
 	// auth
 	auth.Post("/", authcontroller.Login)
@@ -25,6 +27,10 @@ func main() {
 	// product
 	product.Get("/:category_id?", productcontroller.ListProduct)
 	product.Get("/detail/:product_id", productcontroller.DetailProduct)
+
+	// cart
+	cart.Post("/", cartcontroller.ListCart)
+	cart.Post("/add", cartcontroller.AddCart)
 
 	app.Listen(":3000")
 }
