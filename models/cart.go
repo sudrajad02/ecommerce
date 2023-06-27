76e08bfaa78ca7427b9fdc77beb79acd3390d827
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-27 17:56:28 +0700

    Add composite index unique product id and account id in table cart

--- models/cart.go
+++ models/cart.go
@@ -2,10 +2,10 @@ package models
 
 type Cart struct {
 	CartId        int     `gorm:"primary_key;auto_increment" json:"id"`
-	CartProductId int     `gorm:"type:int;null" json:"product_id"`
-	Product       Product `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:CartProductId;references:ProductId"`
-	CartAccountId int     `gorm:"type:int;null" json:"account_id"`
-	Account       Account `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:CartAccountId;references:AccountId"`
+	CartProductId int     `gorm:"type:int;null;index:idx_cart_product,unique" json:"product_id"`
+	Product       Product `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:CartProductId;references:ProductId" json:"product"`
+	CartAccountId int     `gorm:"type:int;null;index:idx_cart_product" json:"account_id"`
+	Account       Account `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:CartAccountId;references:AccountId" json:"account"`
 	AmountProduct int     `gorm:"type:int;null" json:"amount"`
 	TotalPrice    int     `gorm:"type:int;null" json:"price"`
 	CreatedAt     string  `gorm:"type:timestamp;default:null" json:"created_at"`
