d5d22d9e8ab90a13b2bfb0c3a6b53e83110432f3
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-29 00:52:04 +0700

    Add login use token jwt and handler for check jwt in authorization or not

--- controller/authcontroller/authentication.go
+++ controller/authcontroller/authentication.go
@@ -1,12 +1,78 @@
 package authcontroller
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
 	"github.com/sudrajad02/ecommerce/database"
 	"github.com/sudrajad02/ecommerce/models"
 )
 
+func SessionChecker(c *fiber.Ctx) error {
+	envFile := godotenv.Load()
+
+	if envFile != nil {
+		fmt.Print(envFile)
+	}
+
+	var tokenString string
+	authorization := c.Get("Authorization")
+
+	if strings.HasPrefix(authorization, "Bearer") {
+		tokenString = strings.TrimPrefix(authorization, "Bearer ")
+	} else if c.Cookies("token") != "" {
+		tokenString = c.Cookies("token")
+	}
+
+	if tokenString == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "You are not logged in"})
+	}
+
+	jwt_secret := os.Getenv("API_SECRET")
+
+	tokenByte, err := jwt.Parse(tokenString, func(jwtToken *jwt.Token) (interface{}, error) {
+		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %s", jwtToken.Header["alg"])
+		}
+
+		return []byte(jwt_secret), nil
+	})
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": fmt.Sprintf("invalidate token: %v", err)})
+	}
+
+	claims, ok := tokenByte.Claims.(jwt.MapClaims)
+	if !ok || !tokenByte.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "invalid token claim"})
+	}
+
+	var account models.Account
+
+	database.DB.First(&account, "account_id = ?", fmt.Sprint(claims["i"]))
+
+	ourIntegerUser, err := strconv.Atoi(fmt.Sprint(claims["i"]))
+
+	if account.AccountId != ourIntegerUser {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"message": "the user belonging to this token no logger exists"})
+	}
+
+	return c.Next()
+}
+
 func Login(c *fiber.Ctx) error {
+	envFile := godotenv.Load()
+
+	if envFile != nil {
+		fmt.Print(envFile)
+	}
+
 	payload := struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
@@ -30,14 +96,25 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	// claims := jwt.MapClaims{
-	// 	"name":  "John Doe",
-	// 	"admin": true,
-	// 	"exp":   time.Now().Add(time.Hour * 72).Unix(),
-	// }
+	claims := jwt.MapClaims{
+		"i":   account.AccountId,
+		"u":   account.AccountUsername,
+		"exp": time.Now().Add(time.Hour * 24).Unix(), // in hour
+	}
 
-	return c.Status(fiber.StatusOK).JSON(account)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
+	t, err := token.SignedString([]byte(os.Getenv("API_SECRET")))
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"token": t,
+	})
 }
 
 func Register(c *fiber.Ctx) error {
