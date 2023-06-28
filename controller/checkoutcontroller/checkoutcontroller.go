530b1ccb6ed8d91f176cdaa6b97caca4049963a3
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-28 23:58:05 +0700

    Add list checkout by user id and add handle delete cart when success save to checkout

--- controller/checkoutcontroller/checkoutcontroller.go
+++ controller/checkoutcontroller/checkoutcontroller.go
@@ -6,8 +6,25 @@ import (
 	"github.com/sudrajad02/ecommerce/models"
 )
 
+func ListCheckout(c *fiber.Ctx) error {
+	payload := struct {
+		UserId int `json:"account_id"`
+	}{}
+
+	if err := c.BodyParser(&payload); err != nil {
+		return err
+	}
+
+	var checkout []models.Checkout
+
+	database.DB.Where("checkout_account_id = ?", payload.UserId).Preload("Product.ProductCategory").Preload("Account").Preload("AccountAddress").Preload("Product").Find(&checkout)
+
+	return c.Status(fiber.StatusOK).JSON(checkout)
+}
+
 func AddCheckout(c *fiber.Ctx) error {
 	var checkout models.Checkout
+	var cart models.Cart
 
 	if err := c.BodyParser(&checkout); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -21,5 +38,21 @@ func AddCheckout(c *fiber.Ctx) error {
 		})
 	}
 
-	return c.JSON(checkout)
+	payload := struct {
+		CartId int `json:"cart_id"`
+	}{}
+
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	if err := database.DB.Where("cart_id = ?", payload.CartId).Delete(&cart).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(checkout)
 }
