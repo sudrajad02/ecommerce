8d809c1f1b6646c53bfc7c118801fe46cdab4e9b
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-29 01:37:48 +0700

    Add length for password encrypt

--- models/account.go
+++ models/account.go
@@ -5,7 +5,7 @@ type Account struct {
 	AccountName     string           `gorm:"type:varchar(64);null;" json:"name"`
 	AccountEmail    string           `gorm:"type:varchar(64);not null;unique" json:"email"`
 	AccountUsername string           `gorm:"type:varchar(16);not null;unique" json:"username"`
-	AccountPassword string           `gorm:"type:varchar(32);not null;" json:"password"`
+	AccountPassword string           `gorm:"type:varchar(64);not null;" json:"password"`
 	AccountAddress  []AccountAddress `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:AccountAddressAccountId;references:AccountId" json:"account_address"`
 	CreatedAt       string           `gorm:"type:timestamp;default:null" json:"created_at"`
 	UpdatedAt       string           `gorm:"type:timestamp;default:null" json:"updated_at"`
