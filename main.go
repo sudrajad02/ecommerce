8f0a0627e80aaade72320ada6dc6962c94fde5dc
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-06-28 23:38:05 +0700

    Add endpoint for payment and delivery

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/sudrajad02/ecommerce/controller/authcontroller"
 	"github.com/sudrajad02/ecommerce/controller/cartcontroller"
 	"github.com/sudrajad02/ecommerce/controller/checkoutcontroller"
+	"github.com/sudrajad02/ecommerce/controller/deliverycontroller"
 	"github.com/sudrajad02/ecommerce/controller/productcontroller"
 	"github.com/sudrajad02/ecommerce/database"
 )
@@ -22,6 +23,8 @@ func main() {
 	product := api.Group("/product")
 	cart := api.Group("/cart")
 	checkout := api.Group("/checkout")
+	payment := api.Group("/payment")
+	delivery := api.Group("/delivery")
 
 	// auth
 	auth.Post("/", authcontroller.Login)
@@ -37,8 +40,14 @@ func main() {
 	cart.Post("/add", cartcontroller.AddCart)
 	cart.Delete("/delete/:id", cartcontroller.DeleteCart)
 
-	//checkout
+	// checkout
 	checkout.Post("/", checkoutcontroller.AddCheckout)
+
+	// payment
+	payment.Post("/", deliverycontroller.UpdateStatusPayment)
+
+	// delivery
+	delivery.Post("/", deliverycontroller.UpdateStatusDelivery)
 
 	app.Listen(":3000")
 }
